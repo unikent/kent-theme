f098943c691d2dce067505a590f2c4bf7794fde5
gjmh-unikent <g.haddow@kent.ac.uk>
2015-11-05 13:03:57 +0000

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+        copy: {
+            fonts: {
+                expand:true,
+                cwd: 'vendor/kent-font/public/fonts/',
+                src: '**',
+                dest: 'dist/fonts/',
+            },
+        },
+
+        sass: {
+            dist : {
+                files: {
+                    'dist/css/main.css' : 'scss/master.scss'
+                }
+            }
+        },
+        watch: {
+            js: {
+                files: [ 'js/*.js', 'js/**/*.js' ],
+                tasks: [ 'jshint' ]
+            },
+            sass: {
+                files: [ 'scss/*.scss','scss/**/*.scss'  ],
+                tasks: [ 'sass' ]
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                'js/**/*.js'
+            ]
+        },
+        postcss: {
+            options: {
+                map: false,
+
+                processors: [
+                    require('pixrem')(), // add fallbacks for rem units
+                    require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                    //require('cssnano')() // minify the result
+                ]
+            },
+            dist: {
+                src: 'dist/css/*.css'
+            }
+        }
+    });
+
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-postcss');
+
+    // Define tasks
+    grunt.registerTask('development', [ 'jshint', 'copy:fonts', 'sass', 'postcss' ]);
+    grunt.registerTask('default', [ 'development', 'watch' ]);
+};
