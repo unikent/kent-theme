5f9f4a872a9e599ee47cf35e4a68486ef9baf5fd
thybag <thybag@gmail.com>
2015-11-12 16:27:27 +0000

    + the actual page...

---
+++ examples/pg.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+	<head>
+		<title></title>
+		<link rel="stylesheet" href="../dist/css/main_postgraduate.css" />
+		<link rel="stylesheet" href="../dist/css/kentfont.css" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+		<script src="../dist/js/modernizr.min.js"></script>
+	</head>
+	<body>
+		<?php include "inc/header.php"; ?>
+		<main>
+
+			<div class="card card-overlay header-card-overlay">
+				<div class="card-body">
+					<div class="card-media-wrap">
+						<div class="card-embed-wrap embed-responsive embed-responsive-16by9">
+							<video autoplay loop class="embed-responsive-item">
+								<source src="video/research2.mp4">
+							</video>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<section class="container">
+			Ranked 17th for Research Intencity
+			</section>
+
+			<div class="card card-overlay">
+				<div class="card-body card-inverse">
+					<div class="card-title-wrap card-title-wrap-link">
+						<h4 class="card-title">World Leading</h4>
+						<p class="card-text">Our 19 academic schools engage in research of international and world class standing.</p>
+					</div>
+					<div class="card-media-wrap card-media-wrap-video">
+						<img class="card-img-bottom" src="images/students.jpg">
+					</div>
+					
+					<div class="card-img-overlay text-right">
+						<h5 class="card-subtitle">Professor Fox</h5>
+						<p class="card-text">PHD Physics.</p>
+					</div>
+				</div>
+				<div class="card-footer">
+					<a href="#" class="chevron-link">Taught Programmes</a>
+					<a href="#" class="chevron-link">Research Programmes</a>
+					<a href="#" class="chevron-link">PHDs</a>
+					<a href="#" class="chevron-link">Masters</a>
+				</div>
+			</div>
+
+			<div class="card-panel">
+				
+				<div class="card-panel-body">
+					<div class="card">
+						<img class="card-img" src="images/promo-1.jpg">
+						<h3 class="card-title">Global Opportunities</h3>
+						<p class="card-subtitle">1st September 2015</p>
+						<p class="card-text">We rank amongst the best in the UK. Find out why we're more prestigious than many in the Russel Group.</p>
+						<a href="#" class="card-badge kf-comment">5</a>
+					</div>
+					<div class="card">
+						<img class="card-img" src="images/promo-2.jpg">
+						<h3 class="card-title">Scholarships &amp; Funding</h3>
+						<p class="card-subtitle">28th March 2015</p>
+						<p class="card-text">We have a renowned personal teaching style supported by our accessible and passionate academics.</p>
+						<a href="#" class="card-badge kf-comment">7</a>
+					</div>
+					<div class="card">
+						<img class="card-img" src="images/promo-3.jpg">
+						<h3 class="card-title">How to apply</h3>
+						<p class="card-subtitle">5th November 2015</p>
+						<p class="card-text">We'll equip you with essential skills to give you a competitive advantage when it comes to your career.</p>
+						<a href="#" class="card-badge kf-comment">1</a>
+					</div>
+					<div class="card">
+						<img class="card-img" src="images/promo-4.jpg">
+						<h3 class="card-title">Visit Us</h3>
+						<p class="card-subtitle">17th May 2015</p>
+						<p class="card-text">We're ranked one of the best universities for student satisfaction. Learn more why our students love Kent.</p>
+						<a href="#" class="card-badge kf-comment">23</a>
+					</div>
+				</div>
+			</div>
+
+			<blockquote>
+				<img class="img-circle" src="images/profile-academic.jpg"> 
+				<p>"It's the academic community that creates a supportive enviroment to help you excel'"</p>
+				<footer>
+					<cite>
+						Dr David Hume,
+						<span>PhD. Particle physics</span>
+					</cite>
+				</footer>
+			</blockquote>
+
+
+
+<div class="card card-overlay">
+				<div class="card-body card-inverse">
+					<div class="card-title-wrap card-title-wrap-link pull-right">
+						<h4 class="card-title">A video loop</h4>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					</div>
+					<div class="card-media-wrap">
+						<div class="card-embed-wrap embed-responsive embed-responsive-16by9">
+							<video autoplay loop class="embed-responsive-item">
+								<source src="video/research2.mp4">
+							</video>
+						</div>
+					</div>
+					<div class="card-img-overlay">
+						<h5 class="card-subtitle">A sub title</h5>
+						<p class="card-text">This content is shorter.</p>
+					</div>
+				</div>
+			</div>
+
+
+	<div class="card-panel">
+				<div class="card-panel-header">
+					<h2 class="card-panel-title">Standard Card Panel</h2>
+				</div>
+				<div class="card-panel-body">
+					<div class="card">
+						<img class="card-img" src="images/promo-1.jpg">
+						<h3 class="card-title">Academic excellence</h3>
+						<p class="card-subtitle">1st September 2015</p>
+						<p class="card-text">We rank amongst the best in the UK. Find out why we're more prestigious than many in the Russel Group.</p>
+						<a href="#" class="card-badge kf-comment">5</a>
+					</div>
+					<div class="card">
+						<img class="card-img" src="images/promo-2.jpg">
+						<h3 class="card-title">Undergraduates</h3>
+						<p class="card-subtitle">28th March 2015</p>
+						<p class="card-text">We have a renowned personal teaching style supported by our accessible and passionate academics.</p>
+						<a href="#" class="card-badge kf-comment">7</a>
+					</div>
+					<div class="card">
+						<img class="card-img" src="images/promo-3.jpg">
+						<h3 class="card-title">Postgraduates</h3>
+						<p class="card-subtitle">5th November 2015</p>
+						<p class="card-text">We'll equip you with essential skills to give you a competitive advantage when it comes to your career.</p>
+						<a href="#" class="card-badge kf-comment">1</a>
+					</div>
+					<div class="card">
+						<img class="card-img" src="images/promo-4.jpg">
+						<h3 class="card-title">Visit Kent</h3>
+						<p class="card-subtitle">17th May 2015</p>
+						<p class="card-text">We're ranked one of the best universities for student satisfaction. Learn more why our students love Kent.</p>
+						<a href="#" class="card-badge kf-comment">23</a>
+					</div>
+				</div>
+			</div>
+
+		</main>
+		<?php include "inc/footer.php"; ?>
+	
+		<div class="modal fade modal-fullscreen" id="myVideoModal" tabindex="-1" role="dialog" aria-labelledby="myVideoModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+					<div class="modal-content modal-content-transparent">
+
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true"><i class="kf-close"></i></span>
+							<span class="sr-only">Close</span>
+						</button>
+
+						<div class="embed-responsive embed-responsive-16by9">
+							<video controls class="embed-responsive-item">
+								<source src="video/research2.mp4">
+							</video>
+						</div>
+						
+					</div>
+			</div>
+		</div>
+
+		<script src="../dist/js/main.js"></script>
+		<script>
+	</body>
+</html>
