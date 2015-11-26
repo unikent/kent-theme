63324648ee8bd306e337f50f003195b5183db809
jna-unikent <jna@kent.ac.uk>
2015-11-26 11:45:51 +0000

    add more button and make styling work

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9618,3 +9618,31 @@ jQuery(document).ready(function(){
 $('.navbar-toggler').click(function(){
 	$('.navbar-menu').toggleClass('in');
 });
+jQuery(document).ready(function(){
+	var sectional_nav = $('.departmental-nav .navbar-menu');
+	var toggler = $('<span></span>').addClass('navbar-menu-toggler hide').text('More');
+	sectional_nav.append(toggler);
+
+	function respond () {
+		if (viewport.is('>=md') && elementHasOverflown(sectional_nav[0])) {
+			sectional_nav.addClass('overflown');
+			toggler.removeClass('hide');
+		}
+		else {
+			toggler.addClass('hide');
+			sectional_nav.removeClass('overflown');
+		}
+	}
+
+	function elementHasOverflown (el) {
+		return el.offsetHeight < el.scrollHeight || el.offsetWidth < el.scrollWidth;
+	}
+
+	$(window).resize(
+		viewport.changed(function(){
+			respond();
+		})
+	);
+
+	respond();
+});
