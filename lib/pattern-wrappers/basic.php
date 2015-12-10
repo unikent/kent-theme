2c64dfa2efa7b55aae48a9236f318bb70d500a02
thybag <thybag@gmail.com>
2015-12-10 15:45:29 +0000

    update patterns and add js emebed

--- lib/pattern-wrappers/basic.php
+++ lib/pattern-wrappers/basic.php
@@ -2,20 +2,10 @@
 
 include "{bootstrap}";
 
-?>
-<!DOCTYPE html>
-<html lang="en-GB">
-	<head>
+		KentThemeHelper::header("Pattern library", array(
+		'test'=>'test'	
+		),array('title'=>'Pattern library')); ?>
 
-		<link rel="stylesheet" href="<?php KentThemeHelper::getThemeWebRoot();?>assets/css/main.css" />
-		<link rel="stylesheet" href="<?php KentThemeHelper::getThemeWebRoot();?>assets/css/kentfont.css" />
+	{body}
 
-		<script src="<?php KentThemeHelper::getThemeWebRoot();?>assets/js/modernizr.min.js"></script>
-	</head>
-	<body>
-		<main id="main_content">
-			{body}
-		</main>
-		<script src="<?php echo KentThemeHelper::getThemeWebRoot();?>assets/js/main.js"></script>
-	</body>
-</html>
+<?php KentThemeHelper::footer(); ?>
