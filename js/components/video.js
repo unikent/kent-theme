b8d196ce98af93254df9756ed3b452fb9a20f40b
jna-unikent <jna@kent.ac.uk>
2016-01-20 13:06:20 +0000

    only initiate the video modal on click

--- js/components/video.js
+++ js/components/video.js
@@ -2,79 +2,91 @@ $(document).ready(function(){
 	var launcherCount = 0;
 	$('.video-launcher').each(function(){
 
-		// we need some data
-		var modalId = 'videoModal' + launcherCount;
-		var src = $(this).data('src') || false;
-		var transcript = $(this).data('transcript') || false;
+		// initiate and show the video as a modal
+		var init = function () {
+			// we need some data
+			var src = $(this).data('src') || false;
+			var transcript = $(this).data('transcript') || false;
 
-		if (!src) {
-			return;
-		}
-
-		var modal = $(Handlebars.templates.video_modal({
-			id: modalId,
-			src: src,
-			transcript: transcript
-		}));
-
-		var videoEl = modal.find('video');
-
-		if (videoEl.length !== 1) {
-			return;
-		}
-
-		video = videoEl[0];
-
-		$('body').append(modal);
-		modal.modal({show:false});
-
-		var playVideo = function () {
-			if (video.paused) {
-				video.play();
+			if (!src) {
+				return;
 			}
+
+			var modal = $(Handlebars.templates.video_modal({
+				id: this.popupId,
+				src: src,
+				transcript: transcript
+			}));
+
+			var videoEl = modal.find('video');
+
+			if (videoEl.length !== 1) {
+				return;
+			}
+
+			var video = videoEl[0];
+
+			$('body').append(modal);
+			modal.modal({show:false});
+
+			var playVideo = function () {
+				if (video.paused) {
+					video.play();
+				}
+			};
+
+			var pauseVideo = function () {
+				if (!video.paused) {
+					video.pause();
+				}
+			};
+
+			var toggleVideo = function () {
+				if (video.paused) {
+					video.play();
+				}
+				else {
+					video.pause();
+				}
+			};
+
+			// all click handler to parent because thats where out video launcher button will be
+			videoEl.parent().click(function () {
+				toggleVideo();
+			});
+
+			videoEl.on('playing', function (e) {
+				videoEl.parent().removeClass('video-launcher');
+			});
+
+			videoEl.on('pause ended', function (e) {
+				videoEl.parent().addClass('video-launcher');
+			});
+
+			modal.on('shown.bs.modal', function (e) {
+				playVideo();
+			});
+
+			modal.on('hide.bs.modal', function (e) {
+				pauseVideo();
+			});
+
+			this.popup = modal;
+			this.popup.modal('show');
 		};
 
-		var pauseVideo = function () {
-			if (!video.paused) {
-				video.pause();
-			}
-		};
-
-		var toggleVideo = function () {
-			if (video.paused) {
-				video.play();
-			}
-			else {
-				video.pause();
-			}
-		};
-
-		videoEl.click(function () {
-			toggleVideo();
-		});
-
-		videoEl.on('playing', function (e) {
-			videoEl.removeClass('paused');
-		});
-
-		videoEl.on('pause ended', function (e) {
-			videoEl.addClass('paused');
-		});
-
-		modal.on('shown.bs.modal', function (e) {
-			playVideo();
-		});
-
-		modal.on('hide.bs.modal', function (e) {
-			pauseVideo();
-		});
-
-
-		// add click handler to 
-		$(this).click(function () {
-			modal.modal('show');
-		});
-
+		this.popupId = launcherCount;
 		launcherCount += 1;
+
+		// add click handler to initiate/show popup video
+		$(this).click(function () {
+			if (!this.popup) {
+				init.apply(this);
+			}
+			else{
+				this.popup.modal('show');
+			}
+		});
+
 	});
 });
--- js/handlebars_templates.js
+++ js/handlebars_templates.js
@@ -10,9 +10,11 @@ this["Handlebars"]["templates"]["video_modal"] = Handlebars.template({"1":functi
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"modal fade modal-fullscreen\" id=\""
+  return "<div class=\"modal fade modal-fullscreen\" id=\"videoModal"
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myVideoModalLabel\" aria-hidden=\"true\">\r\n	<div class=\"modal-dialog\" role=\"document\">\r\n		<div class=\"modal-content modal-content-transparent\">\r\n\r\n			<div class=\"modal-header\">\r\n				<button type=\"button\" class=\"close pull-right\" data-dismiss=\"modal\" aria-label=\"Close\">\r\n					<span aria-hidden=\"true\"><i class=\"kf-close\"></i></span>\r\n					<span class=\"sr-only\">Close</span>\r\n				</button>\r\n			</div>\r\n\r\n			<div class=\"embed-responsive embed-responsive-16by9 \">\r\n				<video class=\"embed-responsive-item\">\r\n					<source src=\""
+    + "\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"videoModalLabel"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\" aria-hidden=\"true\">\r\n	<div class=\"modal-dialog\" role=\"document\">\r\n		<div class=\"modal-content modal-content-transparent\">\r\n\r\n			<div class=\"modal-header\">\r\n				<button type=\"button\" class=\"close pull-right\" data-dismiss=\"modal\" aria-label=\"Close\">\r\n					<span aria-hidden=\"true\"><i class=\"kf-close\"></i></span>\r\n					<span class=\"sr-only\">Close</span>\r\n				</button>\r\n			</div>\r\n\r\n			<div class=\"embed-responsive embed-responsive-16by9 \">\r\n				<video controls class=\"embed-responsive-item\">\r\n					<source src=\""
     + alias4(((helper = (helper = helpers.src || (depth0 != null ? depth0.src : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"src","hash":{},"data":data}) : helper)))
     + "\" >\r\n				</video>\r\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.transcript : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
