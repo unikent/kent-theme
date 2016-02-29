68f26c9568263d14e2a0a7102608d064f402c36a
thybag <thybag@gmail.com>
2016-02-29 11:09:12 +0000

    add pattern lib bootstrapper

---
+++ lib/pattern-wrappers/pattern_bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+// As terrible as this looks, this code is actually only used via the pattern library build scripts, meaning GET vars
+// are provided solely from the grunt.js build file (which we hopefully trust completely)
+foreach($_GET as $key => $value){
+	define(strtoupper($key), $value);
+}
+
+include "../bootstrap.php";
