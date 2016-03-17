c7c718dff83a78a1b45c3c847ff435bdbf91c7a8
gjmh-unikent <g.haddow@kent.ac.uk>
2016-03-17 14:48:11 +0000

    add login/out scripts

---
+++ public/auth/process.php
@@ -0,0 +1,30 @@
+<?php
+require_once dirname(dirname(dirname(__FILE__)))."/lib/bootstrap.php";
+
+if(defined("ENV") && ENV !=="dev") {
+	$saml_include = defined("SIMPLE_SAML_INCLUDE_PATH")?SIMPLE_SAML_INCLUDE_PATH:"simplesamlphp/lib/_bootstrap.php";
+	require_once $saml_include;
+}
+$returnTo = isset($_REQUEST["returnTo"]) ? $_REQUEST["returnTo"] : HOME_URL;
+
+if(defined("ENV") && ENV !=="dev") {
+	$sp = defined("SIMPLE_SAML_SP") ? SIMPLE_SAML_SP : 'default-sp';
+	$saml = new SimpleSAML_Auth_Simple($sp);
+
+	if($saml->isAuthenticated()) {
+
+		$attrs = $saml->getAttributes();
+
+		setcookie('kent_jwt',$attrs['jwt'],null,"/");
+		header("Location: " . $returnTo);
+		die();
+	}else{
+		header("Location: " . HOME_URL . "/auth/login.php?returnTo=" . $returnTo);
+		die();
+	}
+}else{
+	$jwt = file_get_contents(dirname(dirname(dirname(__FILE__))) . "/jwt_example.json");
+	setcookie('kent_jwt',$jwt,null,"/");
+	header("Location: " . $returnTo);
+	die();
+}
