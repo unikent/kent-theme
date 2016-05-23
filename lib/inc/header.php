299f776335d1f48e77666aabbe80114b7e74bc0e
gjmh-unikent <g.haddow@kent.ac.uk>
2016-05-23 15:05:19 +0100

    make beta link go to blog post always

--- lib/inc/header.php
+++ lib/inc/header.php
@@ -74,7 +74,7 @@
 		<section id="beta-bar" class="beta-bar navbar" role="banner">
 			<p><span class="beta-bar-beta">BETA</span></p>
 
-			<p>We're testing <a href="<?php echo isset($beta_bar['about_url']) ? $beta_bar['about_url'] : 'https://blogs.kent.ac.uk/webdev/2015/12/04/beta-kent/' ?>">a new design</a>.</p>
+			<p>We're testing <a href="https://blogs.kent.ac.uk/webdev/2015/12/04/beta-kent/">a new design</a>.</p>
 			<p>It may not be complete or work as intended.</p>
 			<p class="p-x-1">
 				<a href="<?php echo KentThemeHelper::getOriginalSiteLink();?>" class="">Go to original site</a>
