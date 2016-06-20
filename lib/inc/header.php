fbf993b6f18d04a74210b3f2a5d6652125f0aa44
Dom O'Connor <D.OConnor@kent.ac.uk>
2016-06-20 13:45:36 +0100

    Add hotjar and remove usabilla

--- lib/inc/header.php
+++ lib/inc/header.php
@@ -80,7 +80,7 @@
 				<a href="<?php echo KentThemeHelper::getOriginalSiteLink();?>" class="">Go to original site</a>
 			</p>
 			<p>
-				<button class="btn btn-secondary feedback" onclick="return window.usabilla_live('click');">Give us feedback</button>
+				<button class="btn btn-secondary feedback" href="https://insights.hotjar.com/s?siteId=235141&surveyId=11207">Give us feedback</button>
 			</p>
 			<button class="beta-toggler" aria-expanded="true" aria-controls="beta-bar" title="Hide the Beta bar"></button>
 		</section>
