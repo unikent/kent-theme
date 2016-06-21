1909a61b5eda32c5c20ecfd92846e925d3f46d8d
gjmh-unikent <g.haddow@kent.ac.uk>
2016-06-21 11:28:24 +0100

    fix beta banner and logo placement on chrome (android)

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -28150,9 +28150,9 @@ if(sectional_nav.length===0){return;}function respond(){if(viewport.is('>=md')){
  * Small bit of JS to facilitate the beta bar open/close logic.
  * This will be removed once the product moves out of beta
  */jQuery(document).ready(function(){var beta_bar=$('#beta-bar');var toggler=$('.beta-toggler'); // Toggle beta bar
-function toggleNav(){if(beta_bar.hasClass('hidden')){beta_bar.removeClass('hidden').slideDown();toggler.each(function(){$(this).attr('aria-expanded','true');});}else {beta_bar.addClass('hidden').slideUp();toggler.each(function(){$(this).attr('aria-expanded','false');});window.Cookies.set('kentbeta_dismissed','1',{expires:365});}} // If beta bar exists on page
+function toggleNav(){if(beta_bar.hasClass('hidden')){beta_bar.removeClass('hidden');toggler.each(function(){$(this).attr('aria-expanded','true');});}else {beta_bar.addClass('hidden');toggler.each(function(){$(this).attr('aria-expanded','false');});window.Cookies.set('kentbeta_dismissed','1',{expires:365});}} // If beta bar exists on page
 if(beta_bar.length>0){var dismissed=typeof window.Cookies.get('kentbeta_dismissed')!=='undefined'; // Apply show/hide state from cookie.
-if(!dismissed){beta_bar.removeClass('hidden').slideDown();}else {beta_bar.addClass('hidden').slideUp();} // hook up toggler
+if(!dismissed){beta_bar.removeClass('hidden');}else {beta_bar.addClass('hidden');} // hook up toggler
 toggler.click(function(){toggleNav();});}}); /**
  * Toggles attribution text display on/off
  */jQuery(document).ready(function($){$('.attribution').click(function(){$(this).toggleClass('in');}); // Debug
